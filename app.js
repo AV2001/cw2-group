2fee7424937d86247e57b6f1a61db046d9b753d3
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-01-17 19:27:27 +0400

    add lessons data

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
 
+const lessons = [
+  {
+    topic: 'Math',
+    location: 'Hendon',
+    price: 100,
+  },
+  {
+    topic: 'Math',
+    location: 'Colindale',
+    price: 80,
+  },
+  {
+    topic: 'Math',
+    location: 'Brent cross',
+    price: 90,
+  },
+  {
+    topic: 'Math',
+    location: 'Goders green',
+    price: 120,
+  },
+];
+
 app.listen(3000, () => {
   console.log('Server running at 3000!');
 });
